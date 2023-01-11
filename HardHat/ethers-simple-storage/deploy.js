f1382aaee0249b71ef106b1753b5fb046f61b0fb
Septovan-Dev <septovan.dev@gmail.com>
2023-01-11 13:13:56 +0700

    Complete source codes of ethers.js simple storage

--- HardHat/ethers-simple-storage/deploy.js
+++ HardHat/ethers-simple-storage/deploy.js
@@ -1,4 +1,28 @@
-async function main() {}
+require("dotenv").config();
+const ethers = require("ethers");
+const fs = require("fs");
+
+async function main() {
+  const provider = ethers.providers.JsonRpcProvider(process.env.RPC_URL); // connect to a chain
+  const wallet = ethers.wallet(process.env.WALLET_PRIVATE_KEY, provider);
+  const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
+  const binary = fs.readFileSync(
+    "./SimpleStorage_sol_SimpleStorage.bin",
+    "utf8"
+  );
+  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+  console.log("Deploying, please wait...");
+  const contract = await contractFactory.deploy();
+  await contract.deployTransaction.wait(1);
+
+  const currentFavoriteNumber = await contract.retrieve();
+  console.log(`Current favorite number: ${currentFavoriteNumber.toString()}`);
+
+  const transactionResponse = await contract.store("7");
+  const transactionReceipt = await transactionResponse.wait(1);
+  const updatedFavoriteNumber = await contract.retrieve();
+  console.log(`Updated favorite number: ${updatedFavoriteNumber.toString()}`);
+}
 
 main()
   .then(() => process.exit(0))
